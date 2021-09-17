61e865bfb147b2a87ffd85c0c1551aa629346cf1
christian ferrer <cferrercava@gmail.com>
2021-09-17 09:20:49 -0500

    change build services

--- js.js
+++ js.js
@@ -1,12 +1,1 @@
-
-
-const buildListServices = ()=>{
-    var list = document.getElementById('list-services')
-    for (let index = 0; index < 6; index++) {
-        list.innerHTML += '<li><img src="/images/image_'+(index+1)+'.png" alt=""></li>';        
-    }
-   
-}
-
-
-buildListServices()
+ 
